e21ef57724fca46d21cf439cc0f5f4baf2ecf97d
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-09-07 10:56:15 +0530

    Backend Environment Setup

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config({ path: "./config/config.env" });
+
+export const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
