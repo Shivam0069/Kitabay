f2af5dd118c769381cf0f1a1b5bb05a85294f3f7
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-09-07 11:04:30 +0530

    Database Connection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { connectDB } from "./database/db.js";
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -18,3 +19,5 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+await connectDB();
