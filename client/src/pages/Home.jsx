4b230c479ea75f5a2ca806a873ad49c037580f8c
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-09-08 12:33:52 +0530

    authSlice and Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import SideBar from "../layout/SideBar";
+import AdminDashboard from "../components/AdminDashboard";
+import UserDashboard from "../components/UserDashboard";
+import BookManagement from "../components/BookManagement";
+import Catalog from "../components/Catalog";
+import MyBorrowedBooks from "../components/MyBorrowedBooks";
+
+import Users from "../components/Users";
 
 const Home = () => {
-  return <></>;
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [selectedComponent, setSelectedComponent] = useState("Dashboard");
+
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
+
+  // if (!isAuthenticated) {
+  //   return <Navigate to="/login" />;
+  // }
+
+  return (
+    <div className="relative md:pl-64 flex min-h-screen bg-gray-100">
+      {/* Mobile Hamburger */}
+      <div className="md:hidden z-10 absolute right-6 top-4 sm:top-6 flex justify-center items-center bg-black rounded-md h-9 w-9 text-white">
+        <GiHamburgerMenu
+          className="text-2xl"
+          onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+        />
+      </div>
+
+      {/* Sidebar */}
+      <SideBar
+        isSideBarOpen={isSideBarOpen}
+        setIsSideBarOpen={setIsSideBarOpen}
+        setSelectedComponent={setSelectedComponent}
+      />
+
+      {/* Main Content */}
+      {(() => {
+        switch (selectedComponent) {
+          case "Dashboard":
+            return user?.role === "User" ? (
+              <UserDashboard />
+            ) : (
+              <AdminDashboard />
+            );
+
+          case "Books":
+            return <BookManagement />;
+
+          case "Catalog":
+            if (user?.role === "Admin" || user?.role === "Owner")
+              return <Catalog />;
+            return null;
+
+          case "Users":
+            if (user?.role === "Admin" || user?.role === "Owner")
+              return <Users />;
+            return null;
+
+          case "My Borrowed Books":
+            return <MyBorrowedBooks />;
+
+          default:
+            return user?.role === "User" ? (
+              <UserDashboard />
+            ) : (
+              <AdminDashboard />
+            );
+        }
+      })()}
+    </div>
+  );
 };
 
 export default Home;
---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,369 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: false,
+    error: null,
+    message: null,
+    user: null,
+    isAuthenticated: false,
+  },
+  reducers: {
+    registerRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    registerSuccess: (state, action) => {
+      state.loading = false;
+      //   state.user = action.payload.user;
+      state.message = action.payload.message;
+      //   state.isAuthenticated = true;
+    },
+    registerFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      //   state.isAuthenticated = false;
+    },
+    otpVerificationRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    otpVerificationSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.message = action.payload.message;
+      state.isAuthenticated = true;
+    },
+    otpVerificationFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.isAuthenticated = false;
+    },
+    loginRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.message = action.payload.message;
+      state.isAuthenticated = true;
+    },
+    loginFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.isAuthenticated = false;
+    },
+    logoutRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    logoutSuccess: (state, action) => {
+      state.loading = false;
+      state.user = null;
+      state.message = action.payload;
+      state.isAuthenticated = false;
+    },
+    logoutFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.message = null;
+      //   state.isAuthenticated = false;
+    },
+    getUserRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    getUserSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.isAuthenticated = true;
+    },
+    getUserFailure: (state, action) => {
+      state.loading = false;
+      state.user = null;
+      state.isAuthenticated = false;
+      state.error = action.payload;
+    },
+    forgotPasswordRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    forgotPasswordSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    forgotPasswordFailure: (state, action) => {
+      state.loading = false;
+
+      state.error = action.payload;
+    },
+    resetPasswordRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    resetPasswordSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload.message;
+      state.user = action.payload.user;
+      state.isAuthenticated = true;
+    },
+    resetPasswordFailure: (state, action) => {
+      state.loading = false;
+
+      state.error = action.payload;
+    },
+    updatePasswordRequest: (state) => {
+      state.loading = true;
+      state.message = null;
+      state.error = null;
+    },
+    updatePasswordSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+    updatePasswordFailure: (state, action) => {
+      state.loading = false;
+
+      state.error = action.payload;
+    },
+    reset: (state) => {
+      state.loading = false;
+      state.error = null;
+      state.message = null;
+      state.user = state.user;
+      state.isAuthenticated = state.isAuthenticated;
+    },
+  },
+});
+
+const {
+  registerRequest,
+  registerSuccess,
+  registerFailure,
+  otpVerificationRequest,
+  otpVerificationSuccess,
+  otpVerificationFailure,
+  loginRequest,
+  loginSuccess,
+  loginFailure,
+  logoutRequest,
+  logoutSuccess,
+  logoutFailure,
+  getUserRequest,
+  getUserSuccess,
+  getUserFailure,
+  forgotPasswordRequest,
+  forgotPasswordSuccess,
+  forgotPasswordFailure,
+  resetPasswordRequest,
+  resetPasswordSuccess,
+  resetPasswordFailure,
+  updatePasswordRequest,
+  updatePasswordSuccess,
+  updatePasswordFailure,
+  reset,
+} = authSlice.actions;
+
+export const resetAuthSlice = () => (dispatch) => {
+  dispatch(reset());
+};
+
+export const register = (data) => async (dispatch) => {
+  try {
+    dispatch(registerRequest());
+
+    const { data: response } = await axios.post(
+      "http://localhost:4000/api/v1/auth/register",
+      data,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(registerSuccess(response));
+  } catch (error) {
+    dispatch(
+      registerFailure(
+        error.response?.data?.message || "Registration failed. Try again."
+      )
+    );
+  }
+};
+export const otpVerification = (email, otp) => async (dispatch) => {
+  try {
+    dispatch(otpVerificationRequest());
+
+    const { data: response } = await axios.post(
+      "http://localhost:4000/api/v1/auth/verify-otp",
+      { email, otp },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(otpVerificationSuccess(response));
+  } catch (error) {
+    dispatch(
+      otpVerificationFailure(
+        error.response?.data?.message || "Otp Verification failed. Try again."
+      )
+    );
+  }
+};
+export const login = (data) => async (dispatch) => {
+  try {
+    dispatch(loginRequest());
+
+    const { data: response } = await axios.post(
+      "http://localhost:4000/api/v1/auth/login",
+      data,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(loginSuccess(response));
+  } catch (error) {
+    dispatch(
+      loginFailure(error.response?.data?.message || "Login failed. Try again.")
+    );
+  }
+};
+export const logout = () => async (dispatch) => {
+  try {
+    dispatch(logoutRequest());
+
+    const { data: response } = await axios.get(
+      "http://localhost:4000/api/v1/auth/logout",
+
+      {
+        withCredentials: true,
+      }
+    );
+
+    dispatch(logoutSuccess(response.message));
+    dispatch(reset());
+  } catch (error) {
+    dispatch(
+      logoutFailure(
+        error.response?.data?.message || "Logout failed. Try again."
+      )
+    );
+  }
+};
+export const getUser = () => async (dispatch) => {
+  try {
+    dispatch(getUserRequest());
+
+    const { data: response } = await axios.get(
+      "http://localhost:4000/api/v1/auth/profile",
+
+      {
+        withCredentials: true,
+      }
+    );
+
+    dispatch(getUserSuccess(response));
+  } catch (error) {
+    dispatch(
+      getUserFailure(
+        error.response?.data?.message || "Fetching user failed. Try again."
+      )
+    );
+  }
+};
+export const forgotPassword = (email) => async (dispatch) => {
+  try {
+    dispatch(forgotPasswordRequest());
+
+    const { data: response } = await axios.post(
+      "http://localhost:4000/api/v1/auth/password/forgot",
+      { email },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(forgotPasswordSuccess(response.message));
+  } catch (error) {
+    dispatch(
+      forgotPasswordFailure(
+        error.response?.data?.message ||
+          "Forgot password request failed. Try again."
+      )
+    );
+  }
+};
+export const resetPassword = (data, token) => async (dispatch) => {
+  try {
+    dispatch(resetPasswordRequest());
+
+    const { data: response } = await axios.post(
+      `http://localhost:4000/api/v1/auth/password/reset/${token}`,
+      { data },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(resetPasswordSuccess(response));
+  } catch (error) {
+    dispatch(
+      resetPasswordFailure(
+        error.response?.data?.message ||
+          "Reset password request failed. Try again."
+      )
+    );
+  }
+};
+export const updatePassword = (data) => async (dispatch) => {
+  try {
+    dispatch(updatePasswordRequest());
+
+    const { data: response } = await axios.put(
+      "http://localhost:4000/api/v1/auth/password/update",
+      data,
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch(updatePasswordSuccess(response.message));
+  } catch (error) {
+    dispatch(
+      updatePasswordFailure(
+        error.response?.data?.message ||
+          "Update password request failed. Try again."
+      )
+    );
+  }
+};
+
+export default authSlice.reducer;
