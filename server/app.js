641ccbd96f430b3e89f9704807d13f10ad5f9db6
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2025-09-09 22:15:39 +0530

    CORS url update

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: ["https://kitabay-bookworm.vercel.app"],
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
